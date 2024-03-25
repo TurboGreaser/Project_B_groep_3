fe8f11839b34747db776bfae8bb93868671b2930
mark-175 <152170112+mark-175@users.noreply.github.com>
2024-03-25 12:59:54 +0100

    ListFunctions
    Sort en display method.
    moet nog getest worden.

---
+++ ListFunctions.cs
@@ -0,0 +1,32 @@
+using Project_B;
+public static class ListFunctions
+{
+    public static List<Film> SortList(List<Film> films, string sortBy, bool asc = false)
+    {
+        switch (sortBy)
+        {
+            case "Name":
+                return asc ? films.OrderBy(film => film.Name).ToList() : films.OrderByDescending(film => film.Name).ToList();
+            case "Genre":
+                return asc ? films.OrderBy(film => film.Genre).ToList() : films.OrderByDescending(film => film.Genre).ToList();
+            case "Duration_in_minutes":
+                return asc ? films.OrderBy(film => film.Duration_in_minutes).ToList() : films.OrderByDescending(film => film.Duration_in_minutes).ToList();
+            case "Price":
+                return asc ? films.OrderBy(film => film.Price).ToList() : films.OrderByDescending(film => film.Price).ToList();
+            case "Director":
+                return asc ? films.OrderBy(film => film.Director).ToList() : films.OrderByDescending(film => film.Director).ToList();
+            case "Description":
+                return asc ? films.OrderBy(film => film.Description).ToList() : films.OrderByDescending(film => film.Description).ToList();
+            default:
+                return films;
+        }
+    }
+
+    public static void Display(List<Film> list)
+    {
+        foreach (Film film in list)
+        {
+            Console.WriteLine(film.Info());
+        }
+    }
+}
