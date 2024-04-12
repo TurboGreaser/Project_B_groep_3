c413e4a04b3ff3ba2dbef32af93382904620b09f
ElAllaouiMohamed <miemiet012@gmail.com>
2024-04-12 03:58:43 +0200

    annuleren resrvatie met geld terug en alles nederlands

--- Projce_B_Code/CancelReservation.cs
+++ Projce_B_Code/CancelReservation.cs
@@ -5,30 +5,57 @@ public static class CancelReservation
 {
     public static string jsonfilepath = "Reservations.json";
     public static string jsonfilepathaccounts = "Accounts.json";
+    public static string jsonfilepathfilms = "Films.json";
 
     public static void InfoFromUser()
     {
-        Console.WriteLine("To cancel your reservation, please enter your email and password:");
-        Console.WriteLine("What is your email?");
+        Console.WriteLine("Om uw reservering te annuleren, vul dan de volgende gegevens in:");
+        Console.WriteLine("Wat is uw email?");
         string enteredemail = Console.ReadLine();
-        Console.WriteLine("What is your password?");
+        Console.WriteLine("Wat is uw wachtwoord?");
         string enteredpassword = Console.ReadLine();
 
         bool accountExists = CheckAccountByPassAndEmail(enteredemail, enteredpassword);
 
-        if (accountExists)
+        if (accountExists)// checkt of de account bestaat zoniet dan reservatie niet door
         {
-            Console.WriteLine("You can now cancel your reservation");
-            Console.WriteLine("Please enter your ID and the date:");
-            Console.WriteLine("What is your ID?");
+            bool confirmed = false;
+
+            while (!confirmed)// kijken hvl geld film kost
+            {
+                Console.WriteLine("Wat is de naam van het film?");
+                string movieName = Console.ReadLine();
+
+                double moviePrice = GetMoviePrice(movieName);
+
+                if (moviePrice > 0) // kijken of user eens met geld
+                {
+                    Console.WriteLine($"Geld dat u terug krijgt: {moviePrice}");
+                    Console.WriteLine("Klopt dit? (ja/nee)");
+                    string confirmation = Console.ReadLine().ToLower();
+
+                    if (confirmation == "ja")
+                    {
+                        confirmed = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Film niet gevonden. Vergeet niet de hoofdletters!");
+                }
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Om uw reservering te annuleren, vul uw reserverings ID en de datum in:");
+            Console.WriteLine("Wat is uw reserverings ID?");
             string id_reservation = Console.ReadLine();
-            Console.WriteLine("What is date and time? (YYYY-MM-DD HH:MM)");
+            Console.WriteLine("Wat is de datum van de film? (YYYY-MM-DD HH:MM)");
             string date_reservation = Console.ReadLine();
             RemoveReservationByPassAndId(id_reservation, date_reservation);
         }
         else
         {
-            Console.WriteLine("Account with the entered Email and Password was not found");
+            Console.WriteLine("Account niet gevonden!");
         }
     }
 
@@ -48,11 +75,11 @@ public static class CancelReservation
 
             File.WriteAllText(jsonfilepath, jsontext);
 
-            Console.WriteLine("Your reservation has been removed");
+            Console.WriteLine("Uw reservatie is compleet!");
         }
         else
         {
-            Console.WriteLine("Reservation with the entered ID and date was not found");
+            Console.WriteLine("Uw reservatie is niet gevonden!.");
         }
     }
 
@@ -64,5 +91,24 @@ public static class CancelReservation
         Accounts userToCheck = useraccounts.Find(person => person.Email == email && person.Password == password);
 
         return userToCheck != null;
+    }
+
+
+    public static double GetMoviePrice(string movieName)
+    {
+        string text = File.ReadAllText(jsonfilepathfilms);
+        var films = JsonConvert.DeserializeObject<List<Film>>(text);
+
+        Film movie = films.Find(film => film.Name == movieName); // film zoeken
+
+        if (movie != null)
+        {
+            return movie.Price;
+        }
+        else
+        {
+            Console.WriteLine("Film niet gevonden. Vergeet niet de hoofdletters!");
+            return 0;
+        }
     }
 }
