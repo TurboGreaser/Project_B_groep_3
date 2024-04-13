6aec956da048d56e05fbe1c69803d79f616f7d1c
ElAllaouiMohamed <miemiet012@gmail.com>
2024-04-13 20:38:02 +0200

    korting midden stoelen en reservation class aangemaakt

--- Projce_B_Code/CancelReservation.cs
+++ Projce_B_Code/CancelReservation.cs
@@ -1,5 +1,5 @@
-using Newtonsoft.Json;
-
+using Newtonsoft.Json; 
+using Project_B;
 
 public static class CancelReservation
 {
--- Projce_B_Code/SeatSaleRoom.cs
+++ Projce_B_Code/SeatSaleRoom.cs
@@ -1,3 +1,5 @@
+using Project_B;
+using Newtonsoft.Json;
 class SeatSaleRoom
 {   
     public static string jsonfilepathfilms = "Films.json";
@@ -18,6 +20,57 @@ class SeatSaleRoom
             return 0;
         }
     }
+
+
+
+    public static List<int> GetMiddleSeats(int size)
+    {
+        // int Seats = (int)Math.Pow(size, 2);
+        int Half = size / 2;
+        int quad;
+        if (size % 2 == 0)
+        {
+            quad = size / 4;
+        }
+
+        else
+        {
+            quad =  size / 3;
+            if (quad == 1)
+            {
+                quad ++;
+            }
+        }
+        List<int> FirstRow = new();
+        int Rightmid = Half + quad;
+        List<int> MiddleSeats = new();
+        for (int i = quad; i <= Rightmid; i++)
+        {
+            FirstRow.Add(i);
+            MiddleSeats.Add(i);
+        }
+        int iter = size;
+        for (int i = 0; i < Half -1 ; i++)
+        {
+            foreach (int j in FirstRow)
+            {
+                MiddleSeats.Add(j + iter);
+            }
+            iter += size;
+        }
+
+        return MiddleSeats;
+    }
+
+    public static bool IsExpensive (int size)
+    {
+        int User_seat = SeatSelection.SelectSeat(size, new List<int>{});
+        List<int> SeatsList = GetMiddleSeats(size);
+
+
+        return SeatsList.Contains(User_seat);
+    }
+
 }
 
 
