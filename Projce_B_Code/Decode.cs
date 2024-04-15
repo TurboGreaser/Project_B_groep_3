5ad4b2bbfc0c107419c19fa160291fb4d6a66e20
mark-175 <152170112+mark-175@users.noreply.github.com>
2024-04-15 15:00:10 +0200

    Encode en decode

---
+++ Projce_B_Code/Decode.cs
@@ -0,0 +1,15 @@
+using System.Text;
+public static class Encode 
+{
+    public static string Base64Encode(string plainText)
+    {
+        byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+    
+    public static string Base64Decode(string encodedText)
+    {
+        byte[] encodedTextBytes = Convert.FromBase64String(encodedText);
+        return Encoding.UTF8.GetString(encodedTextBytes);
+    }
+}
