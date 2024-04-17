c9cd3c5401e356a1242dbddd5be32ac23f5dc672
Noellson <1074894@hr.nl>
2024-04-17 13:09:39 +0200

    Create Movies.cs

---
+++ Projce_B_Code/Movies.cs
@@ -0,0 +1,42 @@
+using Newtonsoft.Json;
+
+
+public class Movies
+{
+    public static void Main()
+    {
+        // Datum van vandaag ophalen
+        DateTime today = DateTime.Today;
+
+        // JSON-bestand lezen
+        string json = File.ReadAllText("films.json");
+
+        // Films deserialiseren vanuit JSON
+        List<Film> films = JsonConvert.DeserializeObject<List<Film>>(json);
+
+        // Films voor vandaag identificeren en afdrukken
+        Console.WriteLine("Films die vandaag draaien:");
+
+        foreach (Film film in films)
+        {
+            foreach (KeyValuePair<string, int> showing in film.Showings)
+            {
+                DateTime showingDate = DateTime.Parse(showing.Key);
+
+                if (showingDate.Date == today)
+                {
+                    Console.WriteLine($"- {film.Name}");
+                    break;
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
+
+public class Film
+{
+    public string Name { get; set; }
+    public Dictionary<string, int> Showings { get; set; }
+}
