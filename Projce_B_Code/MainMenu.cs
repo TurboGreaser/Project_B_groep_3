eb02ab11fac10f8f9f4037ab4e089d6d04d6c7b6
mark-175 <152170112+mark-175@users.noreply.github.com>
2024-04-17 13:06:22 +0200

    Main Menu

--- Projce_B_Code/MainMenu.cs
+++ Projce_B_Code/MainMenu.cs
@@ -1,8 +1,8 @@
 using Newtonsoft.Json;
 using Project_B;
-static class MainMenu
+public static class MainMenu
 {
-    private static List<Film> films = Project_B.JsonReader.ReadFilmJson();
+    private static readonly List<Film> films = Project_B.JsonReader.ReadFilmJson();
     
     public static void ShowMenu()
     {
@@ -55,6 +55,54 @@ static class MainMenu
                 ValidInput = false;
             }
 
+        }while(!ValidInput);
+    }
+
+    static void ShowFilmList ()
+    {
+        bool ValidInput = false;
+        ListFunctions.Display(films);
+        string Choice;
+        do
+        {
+            Console.WriteLine("1. Zoeken.\n2. Sorteren.\n3. Terug naar het Menu");
+
+         try
+            {
+                Choice = Console.ReadLine();
+                ValidInput = true;
+                switch (Choice)
+                {
+                    case "1":
+                    Console.Write("Naam van de film: ");
+
+
+                    ValidInput = true;
+                    break;
+                    case "2":
+                    Console.WriteLine("===Reserveren===");
+                    // Reserveren();
+                    ValidInput = true;
+                    break;
+                    case "3":
+                    Console.WriteLine("===Account maken===");
+                    // MakeAccount();
+                    ValidInput = true;
+                    break;
+                    case "4":
+                    Console.WriteLine("===Resturant menu===");
+                    ValidInput = true;
+                    // RestaurantMenu();
+                    break;
+                    default:
+                    Console.WriteLine("Kies tussen 1-4!");
+                    ValidInput = false;
+                    break;
+                }
+
+            }
+            catch(Exception e)
+            {Console.WriteLine($"{e.Message}");}
         }while(!ValidInput);
 
 
