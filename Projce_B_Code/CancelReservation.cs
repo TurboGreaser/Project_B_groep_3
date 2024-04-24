c79aaeb695f8d109b89744c80323eca14ad11759
ElAllaouiMohamed <miemiet012@gmail.com>
2024-04-24 02:37:25 +0200

    cancelreservation

--- Projce_B_Code/CancelReservation.cs
+++ Projce_B_Code/CancelReservation.cs
@@ -1,5 +1,4 @@
 using Newtonsoft.Json;
-using Project_B;
 //using System;
 //using System.Collections.Generic;
 //using System.IO;
@@ -9,62 +8,114 @@ public static class CancelReservation
     public static string jsonfilepath = "Reservations.json";
     public static string jsonfilepathfilms = "Films.json";
 
-
-
     public static void InfoFromUser(string emailToRemove)
     {
-        Console.WriteLine("Weet u zeker dat u uw reservering wilt annuleren? (ja/nee)");
+        string enteredChoice = "";
 
-        string enteredChoice = Console.ReadLine().ToLower();
-
-        if (enteredChoice == "ja")
+        do
         {
-            string text = File.ReadAllText(jsonfilepath);
-            var reservations = JsonConvert.DeserializeObject<List<Reservations>>(text);
+            Console.WriteLine("Weet u zeker dat u uw reservering wilt annuleren? (ja/nee)");
+            enteredChoice = Console.ReadLine().ToLower();
 
-            RemoveReservationByEmail(reservations, emailToRemove);
+            if (enteredChoice == "ja")
+            {
+                string text = File.ReadAllText(jsonfilepath);
+                var reservations = JsonConvert.DeserializeObject<List<Reservations>>(text);
+
+                DisplayReservationsForEmail(reservations, emailToRemove);
+            }
+            else if (enteredChoice != "nee")
+            {
+                Console.WriteLine("Typ 'ja' of 'nee'");
+            }
+            else
+            {
+                Console.WriteLine("Reservering annuleren geannuleerd");
+            }
         }
-        else
-        {
-            Console.WriteLine("Uw reservering is niet verwijderd!");
-        }
+        while (enteredChoice != "ja" && enteredChoice != "nee");
+
     }
 
-
-
-    public static void RemoveReservationByEmail(List<Reservations> reservations, string emailToRemove)
+    public static void DisplayReservationsForEmail(List<Reservations> reservations, string email)
     {
-        foreach (var reservation in reservations)
+        List<Reservations> matchingReservations = new List<Reservations>();
+
+        
+        foreach (var reservation in reservations)// alle reserveringen
         {
-            int index = reservation.Emails.IndexOf(emailToRemove); // kijkt stoel nummer
-            if (index != -1) // stoel gevonden
+            if (reservation.Emails.Contains(email)) // emil adres toevoegen aan list
             {
-                Console.WriteLine($"Reservatie info: {reservation.ID}, Zaal: {reservation.ZaalID}");
-
-                Console.WriteLine("Uw reservering wordt geannuleerd!");
-
-                reservation.Emails.RemoveAt(index);
-                reservation.Seats.RemoveAt(index);
-
-                string jsontext = JsonConvert.SerializeObject(reservations, Formatting.Indented);
-                File.WriteAllText(jsonfilepath, jsontext);
-
-                Console.WriteLine("Uw reservering is geannuleerd!");
-                return;
+                matchingReservations.Add(reservation);
             }
         }
 
-        Console.WriteLine("Uw email is niet gevonden in de reserveringen!");
+        if (matchingReservations.Count == 0)
+        {
+            Console.WriteLine("Geen reserveringen gevonden voor het opgegeven e-mailadres.");
+            return;
+        }
+
+        Console.WriteLine("Reserveringen gevonden voor het opgegeven e-mailadres:");
+        for (int i = 0; i < matchingReservations.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. Reservatie ID: {matchingReservations[i].ID}, Zaal: {matchingReservations[i].ZaalID}");
+        }
+
+        Console.Write("Geef het nummer van de reservering die u wilt annuleren, of typ '0' om te annuleren: ");
+
+        
+        int selectedOption;
+        while (!int.TryParse(Console.ReadLine(), out selectedOption) || selectedOption < 0 || selectedOption > matchingReservations.Count) // verandert naar int en geeft daarna de int door aan selectedoption
+        {
+            Console.WriteLine("Ongeldige invoer. Probeer opnieuw.");
+            Console.Write("Geef het nummer van de reservering die u wilt annuleren, of typ '0' om te annuleren: ");
+        }
+
+        if (selectedOption == 0)
+        {
+            Console.WriteLine("Annulering geannuleerd.");
+            return;
+        }
+
+        Reservations reservationToRemove = matchingReservations[selectedOption - 1];
+        Console.WriteLine($"U heeft gekozen om reservering {reservationToRemove.ID} te annuleren.");
+        RemoveReservation(reservationToRemove);
     }
 
+
+    public static void RemoveReservation(Reservations reservation)
+    {
+        string text = File.ReadAllText(jsonfilepath);
+        var reservations = JsonConvert.DeserializeObject<List<Reservations>>(text);
+
+        // Find the index of the reservation to remove
+        int indexToRemove = reservations.FindIndex(r => r.ID == reservation.ID);
+
+        if (indexToRemove != -1)
+        {
+            // verwijderd van list
+            reservations.RemoveAt(indexToRemove);
+
+            string updatedJson = JsonConvert.SerializeObject(reservations, Formatting.Indented);
+
+            File.WriteAllText(jsonfilepath, updatedJson);
+
+            Console.WriteLine($"Reservatie {reservation.ID} is succesvol geannuleerd!");
+        }
+        else
+        {
+            Console.WriteLine($"Reservatie {reservation.ID} kon niet worden gevonden om te annuleren.");
+        }
+    }
 
 
     public static double GetMoviePrice(string movieName)
     {
         string text = File.ReadAllText(jsonfilepathfilms);
-        var films = JsonConvert.DeserializeObject<List<Project_B.Film>>(text);
+        var films = JsonConvert.DeserializeObject<List<Film>>(text);
 
-        Project_B.Film movie = films.Find(film => film.Name == movieName);
+        Film movie = films.Find(film => film.Name == movieName);
 
         if (movie != null)
         {
