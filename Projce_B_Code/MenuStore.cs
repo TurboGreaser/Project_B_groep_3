1ac4bd0dbb59f1e8e36d1fcc26c0a19b6858bb4f
Noellson <1074894@hr.nl>
2024-04-24 14:53:20 +0200

    menustore

--- Projce_B_Code/MenuStore.cs
+++ Projce_B_Code/MenuStore.cs
@@ -17,6 +17,12 @@ class MenuStore
     {
         Console.WriteLine("----- BIOSCOOP MENU -----");
         Console.WriteLine("Item           Prijs");
+        Console.WriteLine("Popcorn Zout     4,50 Euro");
+        Console.WriteLine("Popcorn Mix      5,50 Euro");
+        Console.WriteLine("Frisdrank        2,00 Euro");
+        Console.WriteLine("Nacho's          5,00 Euro");
+        Console.WriteLine("Hotdog           3,75 Euro");
+        Console.WriteLine("Haribo           1,75 Euro");
         foreach (MenuItem item in menuItems)
         {
             Console.WriteLine(item);
