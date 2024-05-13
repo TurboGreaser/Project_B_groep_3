a8f8f5c5242ba7788e712fba181d795fd8adfc03
ElAllaouiMohamed <miemiet012@gmail.com>
2024-05-13 15:52:37 +0200

    SALE MOVIE

--- Projce_B_Code/SaleMovie.cs
+++ Projce_B_Code/SaleMovie.cs
@@ -1,17 +1,24 @@
-public class SaleMovie
+using Newtonsoft.Json;
+using System.Globalization;
+
+public static class SaleMovie
 {
     private const int SalePercent = 20;
-    public double PriceForMovie { get; set; }
+    public static double TimeSaleStart = 30;
 
-    public SaleMovie(double priceForMovie)
-    {
-        PriceForMovie = priceForMovie;
-    }
 
-    public double CalculateSale(double priceForMovie)
+    public static double CalculateSale(string moviename, DateTime CurrentTime = default)
     {
-        double discountAmount = priceForMovie * SalePercent / 100; // berekent korting
-        return priceForMovie - discountAmount; // geeft de prijs terug
+        string jsonfilepathfilms = "Films.json";
+        string text = File.ReadAllText(jsonfilepathfilms);
+        List<Film>? films = JsonConvert.DeserializeObject<List<Film>>(text);
+
+        Film movie = films.Find(film => film.Name == moviename); // film zoeken
+
+        
+
+        double discountAmount = movie.Price - (movie.Price * SalePercent / 100); // berekent korting
+        return discountAmount; // geeft de prijs terug
 
         // double testprijs = 5; 
         // double korting_test_prijs = SaleMovie.CalculateSale(testprijs);
