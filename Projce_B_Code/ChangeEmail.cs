8139c3cd21c43302156cff3435a2b4a6a79a256d
ElAllaouiMohamed <miemiet012@gmail.com>
2024-05-20 03:37:56 +0200

    (gebruikersacceptatie) word nu gezegd wat fout is ww of email

--- Projce_B_Code/ChangeEmail.cs
+++ Projce_B_Code/ChangeEmail.cs
@@ -1,10 +1,5 @@
+namespace Project_B; 
 using Newtonsoft.Json;
-namespace Project_B;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-
 public static class ChangeEmail
 {
     public static string jsonfilepath = "Accounts.json";
@@ -20,31 +15,52 @@ public static class ChangeEmail
         Console.WriteLine("Wat is je wachtwoord?\n");
         string oldpass = Console.ReadLine();
 
+        bool emailFound = false;
+        bool passwordCorrect = false;
+
         foreach (var user in useraccounts)
         {
-            if (user.Password == Stringcode.Base64Encode(oldpass) && user.Email == email) // werkt niet bij al gemaakte account want die wachtwoorden zijn niet gehashed in de json, als je wel wilt testen haalt dan "Stringcode.Base64Encode" weg
+            if (user.Email == email)
             {
-
-                var new_Mail = AddAccount.GetEmail();
-                while (new_Mail == (null, false))
+                emailFound = true;
+                if (user.Password == Stringcode.Base64Encode(oldpass))
                 {
-                    new_Mail = AddAccount.GetEmail();
+                    passwordCorrect = true;
+
+                    var new_Mail = AddAccount.GetEmail();
+                    while (new_Mail == (null, false))
+                    {
+                        new_Mail = AddAccount.GetEmail();
+                    }
+                    string user_mail = new_Mail.Item1;
+                    bool user_Bool = new_Mail.Item2;
+
+                    user.Email = user_mail;
+                    string json = JsonConvert.SerializeObject(useraccounts, Formatting.Indented);
+                    File.WriteAllText(jsonfilepath, json);
+                    Console.WriteLine("Email succesvol gewijzigd!");
+                    return;
                 }
-                string user_mail = new_Mail.Item1;
-                bool user_Bool = new_Mail.Item2;
-
-                user.Email = user_mail;
-                string json = JsonConvert.SerializeObject(useraccounts, Formatting.Indented);
-                File.WriteAllText(jsonfilepath, json);
-                Console.WriteLine("Email succesvol gewijzigd!");
-
-                return; 
-                
-
-                
+                else
+                {
+                    Console.WriteLine("Het wachtwoord is onjuist. Probeer het opnieuw.\n");
+                    return;
+                }
             }
         }
 
-        Console.WriteLine("Er is geen account in het systeem met het e-mailadres en wachtwoord.");
+        if (!emailFound && !passwordCorrect)
+        {
+            Console.WriteLine("Er is geen account in het systeem met het opgegeven e-mailadres en wachtwoord.");
+        }
+        else if (!passwordCorrect)
+        {
+            Console.WriteLine("Het wachtwoord is onjuist. Probeer het opnieuw.");
+        }
+        else if (!emailFound)
+        {
+            Console.WriteLine("Er is geen account in het systeem met het opgegeven e-mailadres.");
+        }
+
     }
 }
