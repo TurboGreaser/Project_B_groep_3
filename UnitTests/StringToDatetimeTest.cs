cf476dc505e12aeaf11eae375ff6f3b470f4694f
TurboGreaser <creamsupreme666@gmail.com>
2024-06-09 14:45:57 +0200

    stringToDatetime unitTest

---
+++ UnitTests/StringToDatetimeTest.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTests;
+using Project_B;
+
+
+[TestClass]
+public class StringToDatetimeTest
+{
+    [TestMethod]
+    public void TeststringToDateTime()
+    {
+        // string format = "yyyy-MM-dd HH:mm";
+        Assert.AreEqual(new DateTime(2000, 12, 21, 12, 30, 0), Film.StringToDatetime("2000-12-21 12:30"));
+        Assert.AreEqual(new DateTime(2010, 5, 11, 11, 30, 0), Film.StringToDatetime("2010-05-11 11:30"));
+        Assert.AreEqual(new DateTime(2020, 11, 21, 12, 30, 0), Film.StringToDatetime("2020-11-21 12:30"));
+    }
+}
